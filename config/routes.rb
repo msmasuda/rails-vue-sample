5201e91a2f800bf18726f0e0d4e22251b1f4620c
Shinichi Masuda <masuda@my-style.jp>
2018-11-17 14:16:41 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'about/index'
+  # devise_for :users
+  root to: 'about#index'
+  resources :notes
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  # get 'about/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
